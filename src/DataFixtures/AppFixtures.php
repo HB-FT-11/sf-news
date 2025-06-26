99d5dbe5902ad07374468e64f1079140fa1576fd
ld-web <lucas@ld-web.net>
2025-06-26 16:56:29 +0200

    :card_file_box: Category entity, fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,24 +3,41 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private const NB_CATEGORIES = 8;
+    private const NB_ARTICLES = 90;
+
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create('zh_TW');
 
-        for ($i = 0; $i < 50; $i++) {
+        // --- CATEGORIES
+        $categories = [];
+
+        for ($i = 0; $i < self::NB_CATEGORIES; $i++) {
+            $category = new Category();
+            $category->setName($faker->word());
+
+            $manager->persist($category);
+            $categories[] = $category;
+        }
+
+        // --- ARTICLES
+        for ($i = 0; $i < self::NB_ARTICLES; $i++) {
             $article = new Article();
 
             $article
-                ->setTitle($faker->realText(50))
+                ->setTitle($faker->realText(12))
                 ->setContent($faker->realTextBetween(300, 800))
                 ->setCreatedAt($faker->dateTimeBetween('-2 years'))
-                ->setVisible($faker->boolean(80));
+                ->setVisible($faker->boolean(80))
+                ->setCategory($faker->randomElement($categories));
 
             $manager->persist($article); // L'enregistrement n'est pas encore créé
         }
