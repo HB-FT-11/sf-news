0928c2dc8bd5c8ffe0d44c1a7771b635aa00cd6f
ld-web <lucas@ld-web.net>
2025-07-08 10:37:22 +0200

    :sparkles: add EasyAdminBundle
    Article + Category CRUD

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('title'),
+            TextEditorField::new('content')->onlyOnForms(),
+            BooleanField::new('visible'),
+            AssociationField::new('category')->autocomplete()
+        ];
+    }
+}
