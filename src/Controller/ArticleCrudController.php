73841e13ea8c496ca7f0d290a73e61a75e70b864
ld-web <lucas@ld-web.net>
2025-07-09 14:03:59 +0200

    :sparkles: add Access Control (/admin)

--- src/Controller/ArticleCrudController.php
+++ src/Controller/ArticleCrudController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/article/crud')]
+#[Route('/admin/article/crud')]
 final class ArticleCrudController extends AbstractController
 {
     #[Route(name: 'app_article_crud_index', methods: ['GET'])]
